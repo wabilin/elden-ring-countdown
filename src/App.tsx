fc9a46912c59b2ed3c2d267c36be65e4979db911
wabilin <2549826+wabilin@users.noreply.github.com>
2022-02-22 21:43:47 +0900

    adjust style

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,17 @@ const App: Component = () => {
   const diff = () => dayDiff(releaseDate, now());
 
   return (
-    <main>
-      {diff().days}日 {diff().hours}時 {diff().minutes}分 {diff().seconds}秒
+    <main
+      className="flex flex-col justify-center items-center min-h-screen
+      bg-neutral-900 text-[rgb(223,191,111)]"
+    >
+      <h1 className="text-2xl sm:text-4xl my-2 sm:my-4 font-black">エルデンリング発売まで</h1>
+      <strong className="text-xl sm:text-3xl font-mono">
+        {diff().days}日&nbsp;
+        {diff().hours}時&nbsp;
+        {diff().minutes}分&nbsp;
+        {String(diff().seconds).padStart(2, '0')}秒
+      </strong>
     </main>
   );
 };
