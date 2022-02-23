0b5d48ae228528c0a44359b3c7e9006554afbd1a
wabilin <2549826+wabilin@users.noreply.github.com>
2022-02-23 15:37:10 +0900

    handle last day

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,28 @@ const App: Component = () => {
 
   const diff = () => dayDiff(releaseDate, now());
 
+  const diffStr = () => {
+    const {days, hours, minutes, seconds} = diff()
+    const showHours = hours || days;
+    const showMinutes = minutes || showHours;
+    const showSeconds = seconds || showMinutes;
+
+    return [
+      days && `${days}日`,
+      showHours && `${hours}時`,
+      showMinutes && `${minutes}分`,
+      showSeconds && `${seconds.toString().padStart(2, '0')}秒`,
+    ].filter(Boolean).join(' ');
+  }
+
   return (
     <>
       <main className="flex flex-col justify-center items-center min-h-screen">
         <h1 className="text-2xl sm:text-4xl my-2 sm:my-4 font-black">エルデンリング発売まで</h1>
-        <strong className="text-xl sm:text-3xl font-mono">
-          {diff().days}日&nbsp;
-          {diff().hours}時&nbsp;
-          {diff().minutes}分&nbsp;
-          {String(diff().seconds).padStart(2, '0')}秒
+        <strong className="text-xl sm:text-3xl font-mono text-center">
+          {diffStr() || (
+            <span>...発売した！！<br />はやくプレイしよう！！</span>
+          )}
         </strong>
       </main>
       <ForkMe />
